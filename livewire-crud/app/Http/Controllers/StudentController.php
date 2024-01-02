364c5379fad5614359a010f6ab348448f63f26fd
rasedul <rasedul.tatu@gmail.com>
2024-01-02 15:57:02 +0600

    factory create and fake data insert and Create & Index Page design

---
+++ livewire-crud/app/Http/Controllers/StudentController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    public function index(){
+        return view( 'students.index' );
+    }
+
+    public function create(){
+        return view('students.create');
+    }
+}
--- livewire-crud/database/seeders/DatabaseSeeder.php
+++ livewire-crud/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,10 +15,19 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\Classes::factory( 5 )->create();
+        \App\Models\Section::factory( 10 )->create();
+        \App\Models\Student::factory( 10 )->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create( [
+            'name'     => 'Admin',
+            'email'    => 'admin@gmail.com',
+            'password' => Hash::make( '12345678' ),
+        ] );
     }
 }
