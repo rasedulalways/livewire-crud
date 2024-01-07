a756c1606709c7de91d1ea1faa604884d6b3ea6e
rasedul <rasedul.tatu@gmail.com>
2024-01-07 21:14:20 +0600

    Edit update and Delete is done

--- livewire-crud/app/Livewire/TodoList.php
+++ livewire-crud/app/Livewire/TodoList.php
@@ -16,6 +16,11 @@ class TodoList extends Component
 
     public $search;
 
+    public $editingTodoID;
+
+    #[Rule('required|min:3|max:50')]
+    public $editingTodoName;
+
     public function create(){
         $this->validateOnly('name');
 
@@ -28,10 +33,46 @@ class TodoList extends Component
         session()->flash('success', 'Todo save successfully');
     }
 
+    public function delete($todoID){
+        Todo::find($todoID)->delete();
+    }
+
+    // public function delete(Todo $todo){
+    //     $todo->delete();
+    // }
+
+    public function toggle($todoID){
+        $todo = Todo::find($todoID);
+
+        $todo->completed = !$todo->completed;
+        $todo->save();
+    }
+
+    public function edit($todoID){
+        $this->editingTodoID = $todoID;
+
+        $this->editingTodoName = Todo::find($todoID)->name;
+    }
+
+    public function cancelEdit(){
+        $this->reset('editingTodoID', 'editingTodoName');
+    }
+
+    public function update(){
+        $this->validateOnly('editingTodoName');
+
+        Todo::find($this->editingTodoID)->update([
+            'name' => $this->editingTodoName,
+        ]);
+
+        $this->cancelEdit();
+    }
+
+
     public function render()
     {
         return view('livewire.todo-list',[
-            'todos' => Todo::latest()->paginate(5),
+            'todos' => Todo::latest()->where('name', 'like', "%{$this->search}%")->paginate(5),
         ]);
     }
 }
